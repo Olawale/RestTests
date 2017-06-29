194d848618a46df136785e073f5a65a416e14aec
SUP Ola Olanrewaju <SUPOlaola@ad.redbridge.gov.uk>
2017-06-29 13:54:29 +0100

    Aded more test scenarios

--- src/test/java/RestTests/RestApiTest.java
+++ src/test/java/RestTests/RestApiTest.java
@@ -1,9 +1,15 @@
 package RestTests;
 
 import io.restassured.http.ContentType;
+import io.restassured.http.Cookie;
+import io.restassured.http.Header;
+import io.restassured.http.Headers;
+import io.restassured.response.Response;
 import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import static io.restassured.RestAssured.*;
 import static io.restassured.path.json.JsonPath.from;
@@ -20,6 +26,7 @@ public class RestApiTest extends BaseTest{
                 .when().get("/properties")
                 .then()
                 .contentType(ContentType.JSON)
+                .time(lessThan(3l), TimeUnit.SECONDS)
                 .statusCode(200);
     }
 
@@ -88,6 +95,69 @@ public class RestApiTest extends BaseTest{
 
         System.out.print(propIds.size());
         System.out.print(Ids.size());
+    }
+
+    @Test
+    public void testHeaders(){
+
+        Response response = given().auth().basic("simplyrets", "simplyrets")
+                .when().get("/properties")
+                .then()
+                .header("Content-Type", equalTo("application/json; charset=utf-8"))
+                .extract().response();
+//        System.out.println(response.headers().toString());
+//        System.out.println(response.headers().size());
+//        System.out.println(response.header("Content-Type"));
+
+        Headers hd = response.getHeaders();
+        List<String> hd_values = hd.getValues("Content-Type"); // Multivalue headers
+        System.out.println(hd_values.toString());
+
+//        given().auth().basic("simplyrets", "simplyrets")
+//                .when().get("/properties")
+//                .then()
+//                .header("Content-Type", equalTo("application/json; charset=utf-8"));
+
+    }
+
+    @Test
+    public void testCookies(){
+        Response response = given().auth().basic("simplyrets", "simplyrets")
+                .when().get("/properties")
+                .then()
+                .extract().response();
+
+        Map<String, String> allCookies = response.getCookies();
+        System.out.println("All cookies are : " + allCookies.toString());
+
+        Cookie detailedCookie = response.getDetailedCookie("");
+        detailedCookie.getComment();
+    }
+
+    @Test
+    public void testStatus(){
+        Response response = given().auth().basic("simplyrets", "simplyrets")
+                .when().get("/properties")
+                .then()
+                .extract().response();
+
+        String statusLine = response.getStatusLine();
+        int statusCode = response.getStatusCode();
+        System.out.println("The status line is : " + statusLine);
+        System.out.println("The status code is : " + statusCode);
+    }
+
+    @Test
+    public void testResponseTime(){
+        Response response = given().auth().basic("simplyrets", "simplyrets")
+                .when().get("/properties")
+                .then()
+                .extract().response();
+
+        Long respTimeInSeconds = response.timeIn(TimeUnit.SECONDS);
+        Long respTime = response.time();
+        System.out.println("The api response time is : " + respTime);
+        System.out.println("The api response time in SECONDS is : " + respTimeInSeconds);
     }
 
 
