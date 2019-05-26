190bc238113f4249a6c4433334923d75a2a98190
Olawale Olanrewaju <olanrewajume@yahoo.com>
2019-05-26 19:30:34 +0100

    Update BaseTest.java

--- src/test/java/RestTests/BaseTest.java
+++ src/test/java/RestTests/BaseTest.java
@@ -10,24 +10,6 @@ public class BaseTest {
 
     @BeforeClass
     public static void setUp(){
-
-//        String port = System.getProperty("server.port");
-//        if (port == null){
-//            System.out.println("The port env var is " + port);
-//            RestAssured.port = Integer.valueOf(8080);
-//        }
-//        else{
-//            System.out.println("The port env var is " + port);
-//            RestAssured.port = Integer.valueOf(port);
-//        }
-
-//        String baseHost = System.getProperty("server.host");
-//        if(baseHost == null){
-//            System.out.println("The host env var is " + baseHost);
-//            baseHost = "";
-//        }
-//        System.out.println("The host env var is " + baseHost);
-//        RestAssured.baseURI = baseHost;
         RestAssured.baseURI = "https://api.simplyrets.com/";
         RestAssured.authentication = basic("simplyrets", "simplyrets");
         RestAssured.useRelaxedHTTPSValidation();
